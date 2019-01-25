ee9ed0d501648a6e3c782633b6be16c8eabceba4
Alanna G <t_alanna@msn.com>
2019-01-24 21:34:22 -0500

    updates to blog section and single php page info

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 //adds support to show thumbnails of post
 function mytheme_post_thumbnails(){
 	add_theme_support('post-thumbnails' );
+	add_post_type_support('page', 'excerpt' );
 }
 //post thumbnails run feature images
 add_action('after_setup_theme',
